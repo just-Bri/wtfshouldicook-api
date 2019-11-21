8be1b07f46ca8d82568dc061003ffdf99630ad4a
reifnotreef <bridgerrhammond@gmail.com>
2019-11-21 14:15:01 -0600

    rec ing names

--- src/ingredients/ingredients-service.js
+++ src/ingredients/ingredients-service.js
@@ -8,13 +8,13 @@ const IngredientService = {
       .from("recipe_ingredients")
       .where({ recipe_id })
       .innerJoin("ingredients");
-    knex("users").innerJoin("accounts", function() {
-      this.on("accounts.id", "=", "users.account_id").orOn(
-        "accounts.owner_id",
-        "=",
-        "users.id"
-      );
-    });
+    // knex("users").innerJoin("accounts", function() {
+    //   this.on("accounts.id", "=", "users.account_id").orOn(
+    //     "accounts.owner_id",
+    //     "=",
+    //     "users.id"
+    //   );
+    // });
     // join select ing_name from ingredients where id ing.ingredient_id
   }
 };
