41b1c2fcc0fc17983d7b7e2ba4e6cf5faca22344
reifnotreef <bridgerrhammond@gmail.com>
2019-11-21 15:10:37 -0600

    rec ing names

--- src/ingredients/ingredients-service.js
+++ src/ingredients/ingredients-service.js
@@ -6,10 +6,10 @@ const IngredientService = {
     return db
       .from("recipe_ingredients")
       .select("recipe_ingredients.amount")
-      .where("recipe_id", "=", `${recipe_id}`)
-      .join("ingredients", {
-        "recipe_ingredients.ingredient_id": "ingredients.id"
-      });
+      .where("recipe_id", "=", `${recipe_id}`);
+    // .join("ingredients", {
+    //   "recipe_ingredients.ingredient_id": "ingredients.id"
+    // });
     // join select ing_name from ingredients where id ing.ingredient_id
   }
 };
