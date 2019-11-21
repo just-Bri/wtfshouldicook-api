ae621e73aff423284e83320ef9a43066291292a8
reifnotreef <bridgerrhammond@gmail.com>
2019-11-21 14:47:51 -0600

    rec ing names

--- src/ingredients/ingredients-service.js
+++ src/ingredients/ingredients-service.js
@@ -6,7 +6,7 @@ const IngredientService = {
     return knex
       .select("ring.amount", "ing.amount")
       .from("recipe_ingredients AS ring")
-      .innerJoin("ingredients AS ing", "ing.id", "ring.ingredient_id")
+      .join("ingredients AS ing", "ing.id", "ring.ingredient_id")
       .where("ring.recipe_id", "=", recipe_id)
       .returing("*");
     // join select ing_name from ingredients where id ing.ingredient_id
