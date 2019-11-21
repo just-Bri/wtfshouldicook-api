0e9f53a566c9ffb8d3f55049516ee5ba9a44341b
reifnotreef <bridgerrhammond@gmail.com>
2019-11-21 14:50:58 -0600

    rec ing names

--- src/ingredients/ingredients-router.js
+++ src/ingredients/ingredients-router.js
@@ -5,9 +5,9 @@ const IngredientService = require("./ingredients-service");
 const ingredientRouter = express.Router();
 // const jsonParser = express.json();
 
-ingredientRouter.route("/:recipe_id").get((req, res, next) => {
+ingredientRouter.route("/:id").get((req, res, next) => {
   const knexInstance = req.app.get("db");
-  IngredientService.getByRecipeId(knexInstance, req.params.recipe_id)
+  IngredientService.getByRecipeId(knexInstance, req.params.id)
     .then(recipe => {
       console.log(recipe);
       res.json(recipe);
