811acf1cacdc6ed168f74a6328a369b1fc88dc6f
reifnotreef <bridgerrhammond@gmail.com>
2019-11-22 12:15:40 -0600

    trying hardcoded cors rules

--- src/recipe/recipe-router.js
+++ src/recipe/recipe-router.js
@@ -13,56 +13,53 @@ const serializeRecipe = recipe => ({
   cuisine: xss(recipe.cuisine)
 });
 
-recipeRouter
-  .route("/:id")
-  .get((req, res, next) => {
-    const db = req.app.get("db");
-    RecipeService.getById(db, req.params.id)
-      .then(recipe => {
-        res.json(recipe);
-      })
-      .catch(next);
-  })
-  .route("/")
-  .post((req, res, next) => {
-    const db = req.app.get("db");
-    console.log("post recieved, req.body = " + req.body);
-    const {
-      name = recipeDetails[0].name,
-      prep_time = recipeDetails[0].prep_time,
-      cook_time = recipeDetails[0].cook_time,
-      picture_url = recipeDetails[0].picture_url,
-      cuisine = recipeDetails[0].cuisine,
-      complexity = recipeDetails[0].complexity,
-      ingredients = recipeIngredients.forEach(item => {
-        return { name: item.name, amount: item.amount };
-      }),
-      instructions = recipeInstructions.forEach(item => {
-        return {
-          step_number: item.step_number,
-          instructions: item.instructions
-        };
-      })
-    } = req.body;
-    const newRec = {
-      name,
-      prep_time,
-      cook_time,
-      cuisine,
-      complexity,
-      picture_url,
-      ingredients,
-      instructions
-    };
+recipeRouter.route("/:id").get((req, res, next) => {
+  const db = req.app.get("db");
+  RecipeService.getById(db, req.params.id)
+    .then(recipe => {
+      res.json(recipe);
+    })
+    .catch(next);
+});
+recipeRouter.route("/").post((req, res, next) => {
+  const db = req.app.get("db");
+  console.log("post recieved, req.body = " + req.body);
+  const {
+    name = recipeDetails[0].name,
+    prep_time = recipeDetails[0].prep_time,
+    cook_time = recipeDetails[0].cook_time,
+    picture_url = recipeDetails[0].picture_url,
+    cuisine = recipeDetails[0].cuisine,
+    complexity = recipeDetails[0].complexity,
+    ingredients = recipeIngredients.forEach(item => {
+      return { name: item.name, amount: item.amount };
+    }),
+    instructions = recipeInstructions.forEach(item => {
+      return {
+        step_number: item.step_number,
+        instructions: item.instructions
+      };
+    })
+  } = req.body;
+  const newRec = {
+    name,
+    prep_time,
+    cook_time,
+    cuisine,
+    complexity,
+    picture_url,
+    ingredients,
+    instructions
+  };
 
-    RecipeService.postRecipe(db, newRec)
-      .then(() => {
-        res
-          .status(201)
-          .location("/")
-          .json(serializeRecipe(newRec));
-      })
-      .catch(next);
-  });
+  RecipeService.postRecipe(db, newRec)
+    .then(() => {
+      res
+        .status(201)
+        .location("/")
+        .json(serializeRecipe(newRec));
+    })
+    .catch(next);
+});
 
 module.exports = recipeRouter;
