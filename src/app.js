bf454a7cf16569e6912de25f1236932942bd55d7
reifnotreef <bridgerrhammond@gmail.com>
2019-11-22 12:08:17 -0600

    trying hardcoded cors rules

--- src/app.js
+++ src/app.js
@@ -13,6 +13,24 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
