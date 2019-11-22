4f59b264d9089fbade90ac6bafa5ee48de6de2b7
reifnotreef <bridgerrhammond@gmail.com>
2019-11-22 07:20:26 -0600

    working on getting recipe instructions

--- src/instructions/instructions-service.js
+++ src/instructions/instructions-service.js
@@ -3,15 +3,17 @@ const InstructionService = {
     return db.select("*").from("instructions");
   },
   getByRecipeId(db, recipe_id) {
-    return db("recipe_ingredients")
-      .join(
-        "instructions",
-        "recipe_ingredients.instruction_id",
-        "instructions.id"
-      )
-      .select("ingredient_id AS i_id", "step_number", "instructions")
+    return db("instructions")
+      .select("*")
       .where({ recipe_id });
   }
 };
 
 module.exports = InstructionService;
+
+// getByRecipeId(db, recipe_id) {
+//   return db("recipe_ingredients")
+//     .join("ingredients", "recipe_ingredients.ingredient_id", "ingredients.id")
+//     .select("ingredient_amount AS amount", "name")
+//     .where({ recipe_id });
+// }
