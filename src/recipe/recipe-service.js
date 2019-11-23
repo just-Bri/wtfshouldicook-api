be8833fb371428d8b2b816a0c4a6fc660d3bd86d
reifnotreef <bridgerrhammond@gmail.com>
2019-11-23 14:27:15 -0600

    working on recipe insert

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -7,29 +7,27 @@ const RecipeService = {
   },
   postRecipe(db, recipe) {
     // console.log(recipe);
-    return (
-      db("recipes")
-        .insert({
-          name: recipe.name,
-          prep_time: recipe.prep_time,
-          cook_time: recipe.cook_time,
-          cuisine: recipe.cuisine,
-          complexity: recipe.complexity
-        })
-        .returning("id")
-        // .then(console.log)
-        .then(response => {
-          return recipe.instructions.forEach((item, i) => {
-            return {
-              recipe_id: response,
-              step_number: i,
-              instructions: item.step
-            };
-          });
-        })
-        .then(console.log)
-      // .then(db("instructions").insert({ resp }))
-    );
+    return db("recipes")
+      .insert({
+        name: recipe.name,
+        prep_time: recipe.prep_time,
+        cook_time: recipe.cook_time,
+        cuisine: recipe.cuisine,
+        complexity: recipe.complexity
+      })
+      .returning("id")
+      .then(console.log)
+      .then(response => {
+        return recipe.instructions.forEach((item, i) => {
+          return {
+            recipe_id: response,
+            step_number: i,
+            instructions: item.step
+          };
+        });
+      })
+      .then(console.log);
+    // .then(db("instructions").insert({ resp }))
   }
 };
 
