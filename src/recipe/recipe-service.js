00e2c936c2aef3055dbd5f93c0cd246f7df9b2f5
reifnotreef <bridgerrhammond@gmail.com>
2019-11-23 16:19:10 -0600

    working on recipe insert

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -25,7 +25,8 @@ const RecipeService = {
       console.log("inst: " + item.instructions);
       return db("instructions")
         .insert({
-          recipe_id: parseInt(id, 10),
+          recipe_id: 9999,
+          // parseInt(id, 10),
           step_number: i,
           instructions: item.instructions
         })
