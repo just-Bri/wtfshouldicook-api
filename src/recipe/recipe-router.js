cb8d3359a9605729800e2c47e5027fbf34c78f7d
reifnotreef <bridgerrhammond@gmail.com>
2019-11-23 14:37:49 -0600

    working on recipe insert

--- src/recipe/recipe-router.js
+++ src/recipe/recipe-router.js
@@ -21,7 +21,7 @@ recipeRouter
   .post((req, res, next) => {
     const db = req.app.get("db");
     RecipeService.postRecipe(db, req.body).then(response => {
-      res.status(201).send(response);
+      res.status(201).send(console.log(response));
     });
   });
 
