fa13dd00e433a8f47d0ba8d37360657246f7a7c9
reifnotreef <bridgerrhammond@gmail.com>
2019-11-24 11:36:02 -0600

    working on getting ingredients from API

--- src/ingredients/ingredients-service.js
+++ src/ingredients/ingredients-service.js
@@ -4,12 +4,15 @@ const IngredientService = {
   },
   getByRecipeId(db, recipe_id) {
     console.log(`recipe_id in service: ${recipe_id}`);
-    return db("recipe_ingredients")
-      .join("ingredients", "recipe_ingredients.ingredient_id", "ingredients.id")
-      .select("ingredient_amount AS amount", "name")
-      .where({ recipe_id })
-      .returning("*")
-      .then(console.log);
+    // return db("recipe_ingredients")
+    //   .join("ingredients", "recipe_ingredients.ingredient_id", "ingredients.id")
+    //   .select("ingredient_amount AS amount", "name")
+    //   .where({ recipe_id })
+    //   .returning("*")
+    //   .then(console.log);
+    db.raw(
+      "select  ingredient_amount, name from recipe_ingredients JOIN ingredients on ingredients.id = recipe_ingredients.ingredient_id;"
+    );
   }
 };
 
