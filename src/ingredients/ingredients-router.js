9275844a1c4a9b7d3068dfd3c191a6ba7cc472cf
reifnotreef <bridgerrhammond@gmail.com>
2019-11-24 12:44:24 -0600

    working on getting ingredients from API

--- src/ingredients/ingredients-router.js
+++ src/ingredients/ingredients-router.js
@@ -9,10 +9,10 @@ ingredientRouter.route("/:recipe_id").get((req, res, next) => {
   const db = req.app.get("db");
   // console.log(`recipe_id in router: ${req.params.recipe_id}`);
   IngredientService.getByRecipeId(db, req.params.recipe_id)
-    .then(ingredients => {
-      console.log(ingredients);
+    .then(response => {
+      console.log(response);
       // console.log(`response in router.getByRecId: ${response}`);
-      res.json(ingredients);
+      res.json(response);
     })
     .catch(next);
 });
