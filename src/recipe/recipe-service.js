1773b4a941b18bcdae820f3fc9d4e311f69be972
reifnotreef <bridgerrhammond@gmail.com>
2019-11-25 13:07:46 -0600

    testing get without where

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -1,19 +1,19 @@
 const RecipeService = {
   getAllRecipes(db) {
-    return db.select('*').from('recipes');
+    return db.select("*").from("recipes");
   },
   getById(db, id) {
-    return db('recipes').where({ id });
+    return db("recipes").where({ id });
   },
 
   getByAnswers(db, answers) {
     console.log(`answers.cuisine: ${answers.cuisine}`);
     console.log(`answers.complex: ${answers.complex}`);
     return (
-      db('recipes')
-        .count('id as CNT')
+      db("recipes")
+        .count("id as CNT")
         // .where({ complex: answers.complex })
-        .where({ cuisine: answers.cuisine })
+        // .where({ cuisine: answers.cuisine })
         .then(response => {
           let count = Object.values(response[0]);
           let rand = Math.floor(Math.random() * Math.floor(count));
@@ -23,7 +23,7 @@ const RecipeService = {
     );
   },
   postRecipe(db, recipe) {
-    return db('recipes')
+    return db("recipes")
       .insert({
         name: recipe.name,
         prep_time: recipe.prep_time,
@@ -31,36 +31,36 @@ const RecipeService = {
         cuisine: recipe.cuisine,
         complex: recipe.complex
       })
-      .returning('id');
+      .returning("id");
   },
   postRecipeInstructions(db, ins, id) {
     return Promise.all(
       ins.map((item, i) => {
-        return db('instructions')
+        return db("instructions")
           .insert({
             recipe_id: parseInt(id, 10),
             step_number: parseInt(i, 10),
             instructions: item.instructions
           })
-          .returning('recipe_id');
+          .returning("recipe_id");
       })
     ).then(response => response);
   },
   postIngredients(db, ing) {
     return Promise.all(
       ing.map(item => {
-        return db('ingredients')
+        return db("ingredients")
           .insert({
             name: item.name
           })
-          .returning('id');
+          .returning("id");
       })
     ).then(response => response);
   },
   postRecipeIngredients(db, body, id, ing_id) {
     return Promise.all(
       body.ingredients.map((ing, i) => {
-        return db('recipe_ingredients').insert({
+        return db("recipe_ingredients").insert({
           recipe_id: parseInt(id[i], 10),
           ingredient_id: parseInt(ing_id[i], 10),
           ingredient_amount: ing.amount
