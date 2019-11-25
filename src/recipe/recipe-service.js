459c3373dd8ce3b723af267284aa9c11e58ad842
reifnotreef <bridgerrhammond@gmail.com>
2019-11-25 13:42:06 -0600

    logging random

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -9,18 +9,20 @@ const RecipeService = {
   getByAnswers(db, answers) {
     console.log(`answers.cuisine: ${answers.cuisine}`);
     console.log(`answers.complex: ${answers.complex}`);
-    return db("recipes")
-      .where({ complex: answers.complex })
-      .andWhere({ cuisine: answers.cuisine })
-      .count("id as CNT")
-      .then(response => {
-        console.log(`resp from count: ${response[0].CNT}`);
-        let count = Object.values(response[0]);
-        let rand = Math.floor(Math.random() * Math.floor(count)) + 1;
-        console.log(rand);
-        return rand;
-      })
-      .then(rand => rand);
+    return (
+      db("recipes")
+        .count("id as CNT")
+        .where({ complex: answers.complex })
+        // .andWhere({ cuisine: answers.cuisine })
+        .then(response => {
+          console.log(`resp from count: ${response[0].CNT}`);
+          let count = Object.values(response[0]);
+          let rand = Math.floor(Math.random() * Math.floor(count)) + 1;
+          console.log(rand);
+          return rand;
+        })
+        .then(rand => rand)
+    );
   },
   postRecipe(db, recipe) {
     return db("recipes")
