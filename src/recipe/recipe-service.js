9a760e0b449ab9e68dca96c10637a7f1772ac80f
reifnotreef <bridgerrhammond@gmail.com>
2019-11-25 09:15:47 -0600

    working on getting a recipe after answering questions

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -10,7 +10,7 @@ const RecipeService = {
     console.log(`answers.cuisine: ${answers.cuisine}`);
     console.log(`answers.complexity: ${answers.complexity}`);
     // return db("recipes")
-    return answers;
+    return { answers };
   },
   postRecipe(db, recipe) {
     return db("recipes")
