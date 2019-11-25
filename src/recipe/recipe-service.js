6ac4034fd013c9e62677ff177c1dab50c861ec79
reifnotreef <bridgerrhammond@gmail.com>
2019-11-25 10:43:58 -0600

    working on getting a recipe after answering questions

--- src/recipe/recipe-service.js
+++ src/recipe/recipe-service.js
@@ -17,9 +17,8 @@ const RecipeService = {
       .then(response => {
         let count = Object.values(response[0]);
         let rand = Math.floor(Math.random() * Math.floor(count));
-        this.getById(db, rand);
-      })
-      .then(response => response);
+        return this.getById(db, rand);
+      });
     // .then(console.log);
     // .returning("CNT")
     // .then(response => {
