40f3e131085b2f774757a62bc42ef7c0745e92c5
reifnotreef <bridgerrhammond@gmail.com>
2019-11-25 09:38:29 -0600

    working on getting a recipe after answering questions

--- src/recipe/recipe-router.js
+++ src/recipe/recipe-router.js
@@ -17,10 +17,9 @@ let submittedId;
 recipeRouter.route("/").get((req, res, next) => {
   const db = req.app.get("db");
   RecipeService.getByAnswers(db, req.query)
-    .then(response =>
-      res
-        .status(200)
-        .send("res.send from router after service, response: " + response)
+    .then(
+      response => console.log(`response: ${response}`)
+      // res.status(200).send("res.send from router after service")
     )
     .catch(next);
 });
